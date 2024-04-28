e8756536678542baf3de59ececc3e3ceedddcdd3
MANINDRA KUMAR <manindrakumar@MANINDRAs-MacBook-Air.local>
2024-04-28 19:06:45 +0530

    first cdk commit

--- io_t_trigger_lambda_demo/io_t_trigger_lambda_demo_stack.py
+++ io_t_trigger_lambda_demo/io_t_trigger_lambda_demo_stack.py
@@ -1,7 +1,9 @@
 from aws_cdk import (
-    # Duration,
+    RemovalPolicy,
     Stack,
-    # aws_sqs as sqs,
+    aws_lambda, 
+    aws_iot, 
+    aws_iam
 )
 from constructs import Construct
 
@@ -9,11 +11,47 @@ class IoTTriggerLambdaDemoStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        self.build_lambda()
+        self.create_IoT_rule()
+        self.configure_lambda_permission()
 
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "IoTTriggerLambdaDemoQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+    def build_lambda(self):
+        self.lambda_function = aws_lambda.Function(
+            scope=self, 
+            id="IoTTriggeredLambda", 
+            function_name="IoTTriggeredLambda", 
+            code=aws_lambda.Code.from_asset(
+                path="lib"
+            ), 
+            handler="handler.lambda_handler", 
+            runtime=aws_lambda.Runtime.PYTHON_3_12
+        )
+        self.lambda_function.apply_removal_policy(RemovalPolicy.DESTROY)
+        
+    def create_IoT_rule(self):
+        self.iot_rule = aws_iot.CfnTopicRule(
+            scope=self, 
+            id="IOTTriggerLambdaDemoRule",
+            rule_name="IOTTriggerLambdaDemoRule",
+            topic_rule_payload=aws_iot.CfnTopicRule.TopicRulePayloadProperty(
+                actions = [aws_iot.CfnTopicRule.ActionProperty(
+                    lambda_ = aws_iot.CfnTopicRule.LambdaActionProperty(
+                        function_arn=self.lambda_function.function_arn
+                    )
+                )], 
+                sql="SELECT topic(2) as region, * FROM 'temperature/#' WHERE temperature > 35",
+                aws_iot_sql_version = "2016-03-23", 
+                rule_disabled=False
+            )
+        )
+        self.iot_rule.apply_removal_policy(RemovalPolicy.DESTROY)
+        
+        
+    def configure_lambda_permission(self):
+        self.lambda_function.add_permission(
+            scope=self,
+            id="IOTTriggerLambdaDemoRulePermission", 
+            principal=aws_iam.ServicePrincipal("iot.amazonaws.com"),
+            source_arn=self.iot_rule.attr_arn,
+            action="lambda:InvokeFunction"
+        )
